92078ad2fbdd817e109c218bef1b53b3f434835f
Egor Danilov <159701362+kimmito@users.noreply.github.com>
2026-04-14 21:08:12 +0300

    chore(client): fix alias resolution for typescript and vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
@@ -12,7 +13,12 @@ export default defineConfig({
 	],
 	resolve: {
 		alias: {
-			'@': '/src'
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+			'@components': fileURLToPath(
+				new URL('./src/components', import.meta.url)
+			),
+			'@screens': fileURLToPath(new URL('./src/screens', import.meta.url)),
+			'@navigation': fileURLToPath(new URL('./src/navigation', import.meta.url))
 		}
 	}
 })
